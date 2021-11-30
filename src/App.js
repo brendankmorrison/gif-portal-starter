a8d011e2da3605c480553a55b8a04e0ba582d6fb
Brendan <b.k.morrison2@gmail.com>
2021-11-30 12:42:24 -0800

    add phantom wallet detection

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -6,6 +7,32 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const {solana} = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
   return (
     <div className="App">
       <div className="container">
