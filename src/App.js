2069cd9a3d69b2302b7f46d56878906acd1c1e0a
Brendan <b.k.morrison2@gmail.com>
2021-11-30 13:01:50 -0800

    add connect wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -7,6 +7,7 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
 
   useEffect(() => {
     const onLoad = async () => {
@@ -23,6 +24,12 @@ const App = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log("Phantom wallet found!");
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert("Solana object not found! Get a Phantom Wallet.");
@@ -32,24 +39,49 @@ const App = () => {
     }
   }
 
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className = "cta-button connect-wallet-button"
+      onClick = {connectWallet}
+    >
+      Connect to Wallet
+    </button>
+
+  );
+
   
   return (
     <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+      {/* This was solely added for some styling fanciness */}
+			<div className={walletAddress ? 'authed-container' : 'container'}>
+        <div className="container">
+          <div className="header-container">
+            <p className="header">🖼 GIF Portal</p>
+            <p className="sub-text">
+              View your GIF collection in the metaverse ✨
+            </p>
+            {/* render only if we do not have a wallet address */}
+            {!walletAddress && renderNotConnectedContainer()}
+          </div>
+          <div className="footer-container">
+            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+          </div>
         </div>
       </div>
     </div>
